c997c28b219d61b8c6636245dad5dcf7eca5849a
Nguyen Nhut Huy <133396150+NhutHuyDev@users.noreply.github.com>
2024-02-18 15:26:17 +0700

    Delete be directory

--- be/src/utils/customHttpHeaders.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  API_KEY: 'x-api-key',
-  CLIENT_ID: 'x-client-id',
-  REFRESH_TOKEN: 'x-refresh'
-}
--- be/src/utils/httpStatusRespones/index.ts
+++
@@ -1,5 +0,0 @@
-import statusCode from './statusCodes'
-import defaultStatusMsg from './defaultStatusMsg'
-
-export const STATUS_CODE = statusCode
-export const DEFAULT_STATUS_MESSAGE = defaultStatusMsg
